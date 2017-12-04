aa718d3c7090364a318c0b4ac147a6eec82345b4
redo17 <redouane.tigrara@ent.univ-amu.fr>
2017-12-04 18:14:08 +0100

    pttr mailsender

--- src/mail/manager/MailTest.java
+++ src/mail/manager/MailTest.java
@@ -7,18 +7,13 @@ public class MailTest {
  
     public static void main(String[] args)
     {
-        //Create the application context
-    	System.out.print("Ok1");
+
         ApplicationContext context = new FileSystemXmlApplicationContext("WebContent/WEB-INF/springapp-servlet.xml");
-        System.out.print("Ok2");
-        //Get the mailer instance
         ApplicationMailer mailer = (ApplicationMailer) context.getBean("mailService");
- 
-        //Send a composed mail
-//        mailer.sendMail("somebody@gmail.com", "Test Subject", "Testing body");
-        System.out.print("Ok3");
-        //Send a pre-configured mail
-        mailer.sendPreConfiguredMail("Exception occurred everywhere.. where are you ????");
-        System.out.print("OOOOK");
+        String to = "redouane.tigrara@gmail.com";
+        String subject = "Récupération de votre mot de passe";
+        String body = "dtc";
+        mailer.sendMail(to, subject, body);
+
     }
 }
